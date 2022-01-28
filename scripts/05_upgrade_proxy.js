c758478a0b3075c04fcdd4664976d43dab9fef20
wecare-lab <dev@wecarelabs.org>
2022-01-28 19:25:14 +0100

    v1.0.5-transfer-balance
    transfer balance address changed to marketing wallet

--- scripts/05_upgrade_proxy.js
+++ scripts/05_upgrade_proxy.js
@@ -2,7 +2,7 @@ const { ethers, upgrades } = require("hardhat");
 
 async function main() {
   const Portoken = await ethers.getContractFactory("PorToken");
-  const Proxy = await upgrades.upgradeProxy("0x9000Cac49C3841926Baac5b2E13c87D43e51B6a4", Portoken);
+  const Proxy = await upgrades.upgradeProxy("0x9000Cac49C3841926Baac5b2E13c87D43e51B6a4", Portoken, {kind: 'transparent'});
 
   console.log("POR upgraded");
   console.log("Your upgraded proxy is done!", Proxy.address)
